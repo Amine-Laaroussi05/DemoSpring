8306570d6790d0a2611548c30fe9bd7e812a03c0
Amine Laaroussi <amine.laaroussi05@gmail.com>
2022-06-23 12:20:34 +0200

    feat: Mapping des objets :sparkles:

--- src/main/java/ib/demoSpring/entity/Guitare.java
+++ src/main/java/ib/demoSpring/entity/Guitare.java
@@ -1,7 +1,17 @@
 package ib.demoSpring.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Guitare implements Instrument{
 
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "modelGuitare")
     private String model;
 
     public Guitare() {
@@ -11,12 +21,25 @@ public class Guitare implements Instrument{
         this.model = model;
     }
 
+    public Guitare(Long id, String model) {
+        this.id = id;
+        this.model = model;
+    }
+
     public String getModel() {
         return model;
     }
 
     public void setModel(String model) {
         this.model = model;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     @Override
