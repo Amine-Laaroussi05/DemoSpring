7310472c448a1d59690733f8a19b003b8ebaccc8
Amine Laaroussi <amine.laaroussi05@gmail.com>
2022-06-23 16:21:39 +0200

    feat: Ajout d'une query dans un dao :sparkles:

--- src/main/java/ib/demoSpring/dao/SonneurDAO.java
+++ src/main/java/ib/demoSpring/dao/SonneurDAO.java
@@ -2,6 +2,15 @@ package ib.demoSpring.dao;
 
 import ib.demoSpring.entity.Sonneur;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SonneurDAO extends JpaRepository<Sonneur,Long> {
+
+    @Query("SELECT s FROM sonneur s WHERE s.nom = :nom")
+    public List<Sonneur> findByNom(@Param("nom") String nom);
+
+
 }
